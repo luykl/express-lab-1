10d86a70fa4b2d3e05aac0df4c33b73f95be20cd
luykl <leah.logsin@gmail.com>
2021-05-06 16:44:11 -0400

    fixed cors

--- index.ts
+++ index.ts
@@ -3,10 +3,10 @@ import routes from './cart-routes';
 import cors from 'cors';
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 app.use("/", routes);
-app.use(cors());
+
 
 const port = 3000;
 app.listen(port, () => {
